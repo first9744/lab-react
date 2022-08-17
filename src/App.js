1b0460c2122da0fbe193d48cf3b33b837e177f40
first9744 <firstfirst9977@gmail.com>
2022-08-18 01:43:10 +0700

    Assignment 7.1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const changeword = () => {
+  const word2 = word;
+}
+
 function App() {
   return (
     <div>
-
       <WordCard value={word}/>
+      <h4>Explanation : Arrange the English letters in the correct order.</h4>
     </div>
   );
 }
+
+
 export default App;
